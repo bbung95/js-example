2e4460f8930caa63d10d64711f403dfde0cc17b4
rai <tjd123123@naver.com>
2023-01-13 16:40:21 +0900

    refactor : market-example fetch 콜백 수정
    fetch API 콜백함수 => async await 변경

--- js_basic_market/main.js
+++ js_basic_market/main.js
@@ -1,5 +1,6 @@
 import { getListSection } from "./module/productSection.js";
 
+/* 콜백함수 사용
 let listData;
 
 const initListData = (data) => {
@@ -17,3 +18,17 @@ fetch("./public/mock/sectionListData.json")
     .then((data) => {
         initListData(data);
     });
+*/
+
+// async await 사용
+try {
+    const result = await fetch("./public/mock/sectionListData.json");
+    const data = await result.json();
+    const sectionInfoList = data.sectionInfoList;
+
+    sectionInfoList.forEach((item) => {
+        document.body.appendChild(getListSection(item.sectionTitle, item.productList));
+    });
+} catch (error) {
+    console.log(error);
+}
