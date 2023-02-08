1d18d668076065ba09b60f62fe802b0e5bf49faa
rai <tjd123123@naver.com>
2023-02-08 16:21:22 +0900

    feat : js-youtube-clone 검색추가
    search 함수 추가
    contents hover 미리보기 추가

--- js_youtube_clone/script.js
+++ js_youtube_clone/script.js
@@ -1,3 +1,48 @@
-;(function () {
-  'use strict'
-})()
+(function () {
+    "use strict";
+
+    const get = (target) => document.querySelector(target);
+    const getAll = (target) => document.querySelectorAll(target);
+
+    const $searchInput = get("#search");
+    const $searchBtn = get(".btn_search");
+
+    const $contentList = getAll(".contents.list figure");
+
+    const searchContents = () => {
+        const keyword = $searchInput.value;
+
+        $contentList.forEach((item) => {
+            const title = item.querySelector(".description strong").textContent;
+            if (title.includes(keyword)) {
+                item.style.display = "flex";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    };
+
+    const onMouseover = (e) => {
+        const webpPlay = e.target.parentNode.querySelector("source");
+        webpPlay.setAttribute("srcset", "./assets/sample.webp");
+    };
+
+    const onMouseout = (e) => {
+        const webpPlay = e.target.parentNode.querySelector("source");
+        webpPlay.setAttribute("srcset", "./assets/sample.jpg");
+    };
+
+    const init = () => {
+        $searchBtn.addEventListener("click", searchContents);
+
+        console.log($contentList);
+
+        $contentList.forEach((item) => {
+            const $target = item.querySelector("picture");
+            $target.addEventListener("mouseover", onMouseover);
+            $target.addEventListener("mouseout", onMouseout);
+        });
+    };
+
+    init();
+})();
