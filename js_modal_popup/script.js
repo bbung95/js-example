d8fddee7cfa1b94448def981527a9815ff368429
rai <tjd123123@naver.com>
2023-02-23 18:57:09 +0900

    feat : js-modal-popup dialog
    dialog tag modal
    form tag method를 이용한 dialog 조작

--- js_modal_popup/script.js
+++ js_modal_popup/script.js
@@ -5,6 +5,10 @@
     };
 
     const $body = get("body");
+
+    /**
+     *  toggle을 사용한 modal popup
+     */
     const $modalBtn = get(".modal_open_button");
     const $modal = get(".modal");
     const $modalConfirmBtn = get(".modal_button.confirm");
@@ -20,7 +24,23 @@
     $modalConfirmBtn.onclick = () => toggleModal();
     $modalCloseBtn.onclick = () => toggleModal();
 
+    /**
+     * dialog를 이용한 modal popup
+     */
+    const $dialogBtn = get(".dialog_open_button");
+    const $dialog = get("dialog");
+    const $dialogConfirmBtn = get(".dialog_button.confirm");
+    const $dialogCloseBtn = get(".dialog_button.cancel");
+
+    $dialogBtn.onclick = () => {
+        $dialog.showModal();
+        $body.classList.toggle("scroll_rock");
+    };
+
+    $dialog.onclose = () => $body.classList.toggle("scroll_rock");
+
     window.onclick = (e) => {
         if (e.target === $modal) toggleModal();
+        if (e.target.nodeName === "DIALOG") $dialog.close();
     };
 })();
